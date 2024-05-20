d1b08bbe4d588e35515c9929821761155a4d3b29
Kethelyn Carvalho <kethelyncrb@gmail.com>
2024-05-20 16:53:13 -0300

    Alteração de código, adicionando comentários e reduzindo o código

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-function tocaSom(idElementoAudio) { // Função para reproduzir som
-    document.querySelector(idElementoAudio).play(); // Reproduz o som do elemento de áudio com o ID especificado
-}
+function tocaSom(seletorAudio) { // Função para reproduzir som
+    // Verifica se o seletorAudio é válido e se o elemento é um elemento de áudio
+    const elemento = document.querySelector(seletorAudio);
+    if (elemento && elemento.localName === 'audio') {
+      // Se o elemento for válido e for um elemento de áudio, reproduz o som
+      elemento.play();
+    } else {
+      // Se o elemento não for válido ou não for um elemento de áudio, exibe uma mensagem de erro no console
+      console.log('Elemento não encontrado ou seletor inválido');
+    }
+  }
   
-  const listaDeTeclas = document.querySelectorAll('.tecla'); // Obtém uma lista de todos os elementos com a classe 'tecla'
+  // Obtém uma lista de todos os elementos com a classe 'tecla'
+  const listaDeTeclas = document.querySelectorAll('.tecla');
   
-  // Loop para cada tecla
+  // Loop para cada tecla na listaDeTeclas
   for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador]; // Armazena o elemento da tecla atual
     const instrumento = tecla.classList[1]; // Obtém o nome do instrumento da classe da tecla (segunda classe)
@@ -14,21 +23,20 @@ function tocaSom(idElementoAudio) { // Função para reproduzir som
     tecla.onclick = function() {
       tocaSom(idAudio); // Chama a função tocaSom com o ID do elemento de áudio do instrumento atual
     };
-
-    tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space') {
+  
+    // Configura um manipulador de evento 'onkeydown' para a tecla
+    tecla.onkeydown = function(evento) {
+      // Verifica se a tecla pressionada é 'Space' ou 'Enter'
+      if (evento.code === 'Space' || evento.code === 'Enter') {
+        // Adiciona a classe 'ativa' à tecla quando pressionada
         tecla.classList.add('ativa');
-        }
-
-        if (evento.code === 'Enter') {
-            tecla.classList.add('ativa')
-
-        }
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-}
+      }
+    };
+  
+    // Configura um manipulador de evento 'onkeyup' para a tecla
+    tecla.onkeyup = function() {
+      // Remove a classe 'ativa' da tecla quando solta
+      tecla.classList.remove('ativa');
+    };
+  }
   
